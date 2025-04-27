56f79c521a9295cda99acdef5c0a74e3917378d1
Sudip Sharma <sudpokhara826@gmail.com>
2025-04-27 22:32:19 +0545

    Added MVCR ,Midlleware and the initalizitzed deloment of backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 
@@ -8,6 +9,15 @@ const app = express();
 
 //Port number to run the server
 const PORT = process.env.PORT 
+
+//Middleware 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+
+}))
 
 //Testing Routes
 app.get('/', (req, res) => {

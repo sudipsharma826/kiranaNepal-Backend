3324fc0a7dc5de1507e82750aadefbfb1ae8bae0
Sudip Sharma <sudpokhara826@gmail.com>
2025-04-28 22:42:06 +0545

    Completed the curd oepration in the product side

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 import products from '../models/productModel.js';
-import { uploadToCloudinary } from '../utils/cloudinary.js';
+import { deleteFromCloudinary, uploadToCloudinary } from '../utils/cloudinary.js';
 import { sendResponse } from "../utils/response.js";
 
 export const addProduct = async (req, res) => {
@@ -26,6 +26,7 @@ export const addProduct = async (req, res) => {
         }
 
         const slug = name.replace(/\s+/g, '-').toLowerCase();
+        let tagArray = Array.isArray(tags) ? tags : tags.split(',').map(tag => tag.trim()); //Convert the string list into the array
 
         const newProduct = await products.create({
             name,
@@ -35,7 +36,7 @@ export const addProduct = async (req, res) => {
             reviews,
             image: imageURL.secure_url,
             description,
-            tags,
+            tags: tagArray,
             stock,
             slug,
         });
@@ -43,10 +44,106 @@ export const addProduct = async (req, res) => {
         sendResponse(res, 201, true, "Product added successfully", newProduct);
     } catch (error) {
         if(error.code === 11000) {
-            sendResponse(res, 400, false, "Product already exists with this name or ", error.message);
+            sendResponse(res, 400, false, "Product already exists with this name ", error.message);
             return;
         }
         console.error('Error:', error);
         sendResponse(res, 500, false, "Internal server error", error.message);
     }
+};
+
+
+//Get all products
+export const getAllProducts = async (req, res) => {
+    try {
+        const productsList = await products.find({});
+        sendResponse(res, 200, true, "Products fetched successfully", productsList);
+    } catch (error) {
+        console.error('Error:', error);
+        sendResponse(res, 500, false, "Internal server error", error.message);
+    }
+};
+
+//Get single product
+export const getSingleProduct = async (req, res) => {
+    try {
+        const { slug } = req.params;
+        const product = await products.findOne({slug : slug});
+        if (!product) {
+            sendResponse(res, 404, false, "Product not found");
+            return;
+        }
+        sendResponse(res, 200, true, "Product fetched successfully", product);
+    }
+    catch (error) {
+        console.error('Error:', error);
+        sendResponse(res, 500, false, "Internal server error", error.message);
+    }
 }
+
+//Update the product
+export const updateProduct = async (req, res) => {
+    try {
+        const { slug } = req.params;
+        const { name, category, price, rating, reviews, description, tags, stock } = req.body;
+        const uploadedImage = req.file;
+
+        if (!name || !category || !price || !rating || !reviews || !description || !tags || !stock) {
+            sendResponse(res, 400, false, "Please fill all the fields");
+            return;
+        }
+
+        let imageURL;
+        if (uploadedImage) {
+            imageURL = await uploadToCloudinary(uploadedImage);
+            if (!imageURL) {
+                sendResponse(res, 500, false, "Image upload failed");
+                return;
+            }
+        }
+
+        const updatedProduct = await products.findOneAndUpdate({slug}, {
+            name,
+            category,
+            price,
+            rating,
+            reviews,
+            description,
+            tags,
+            stock,
+            ...(imageURL && { image: imageURL.secure_url })
+        }, { new: true });
+
+        if (!updatedProduct) {
+            sendResponse(res, 404, false, "Product not found");
+            return;
+        }
+
+        sendResponse(res, 200, true, "Product updated successfully", updatedProduct);
+    } catch (error) {
+        console.error('Error:', error);
+        sendResponse(res, 500, false, "Internal server error", error.message);
+    }
+}
+
+//Delete the product
+export const deleteProduct = async (req, res) => {
+    try {
+        const { slug } = req.params;
+        const deletedProduct = await products.findOneAndDelete({slug});
+        if (!deletedProduct) {
+            sendResponse(res, 404, false, "Product not found");
+            return;
+        }
+        //Deleted the image from the cloudinary
+        if (deletedProduct) {
+            await deleteFromCloudinary(deletedProduct.image);
+        }
+        sendResponse(res, 200, true, "Product deleted successfully", deletedProduct);
+    }
+    catch (error) {
+        console.error('Error:', error);
+        sendResponse(res, 500, false, "Internal server error", error.message);
+    }
+}
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { upload } from '../middleware/multer.js';
-import { addProduct } from '../controllers/productController.js';
+import { addProduct, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from '../controllers/productController.js';
+import authSeller from '../middleware/sellerMiddleware.js';
 
 
 const productRouter = express.Router();
-productRouter.post('/add_product',upload.single('image'),addProduct);
+productRouter.post('/add_product',upload.single('image'),authSeller,addProduct);
+productRouter.get('/get_products',getAllProducts);
+productRouter.get('/get_product/:slug',getSingleProduct); 
+productRouter.put('/update_product/:slug',upload.single('image'),authSeller,updateProduct); 
+productRouter.post('/delete_product/:slug',authSeller,deleteProduct);
 
 export default productRouter;

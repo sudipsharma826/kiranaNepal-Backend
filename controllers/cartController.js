eeb0311ef225bca0ad7e0b1f5e64d7f791e1081b
Sudip Sharma <sudpokhara826@gmail.com>
2025-05-04 20:28:10 +0545

    added the get cart and updated cart funcitility

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,24 +1,35 @@
-import user from "../models/userModel.js";
+import User from "../models/userModel.js";
 import { sendResponse } from "../utils/response.js";
-export const update_cart = async (req, res) => {
-    const {cartItems} = req.body;
-    const userId = req.userId;
-    if (!userId || !cartItems) {
-        sendResponse(res, 400, false, "Please fill all the fields");
-        return;
-    }
-    try {
-        const user = await user.findById(userId);
-        if (!user) {
-            sendResponse(res, 404, false, "User not found");
-            return;
-        }
-        user.cart = cartItems;
-        await user.save();
-        sendResponse(res, 200, true, "Cart updated successfully", user.cart);
-    } catch (error) {
-        console.error('Error:', error);
-        sendResponse(res, 500, false, "Internal server error", error.message);
-    }
 
-}
+
+//Get the cart items of the user
+export const get_cart = async (req, res) => {
+    const userId=req.userId;
+  try {
+    const user = await User.findById(userId).select("cartItems");
+    if (!user) {
+      return sendResponse(res, 404, false, "User not found", null);
+    }
+    return sendResponse(res, 200, true, "Cart items fetched successfully", user.cartItems);
+  } catch (error) {
+    return sendResponse(res, 500, false, "Server error", error.message);
+  }
+};
+
+//Update the cart items of the user
+export const update_cart = async (req, res) => {
+  const userId=req.userId;
+  const { cartItems } = req.body;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return sendResponse(res, 404, false, "User not found", null);
+    }
+    user.cartItems = cartItems;
+    await user.save();
+    return sendResponse(res, 200, true, "Cart items updated successfully", user.cartItems);
+    }
+    catch (error) {
+    return sendResponse(res, 500, false, "Server error", error.message);
+    }
+};
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { update_cart } from '../controllers/cartController.js';
+import { get_cart, update_cart } from '../controllers/cartController.js';
 import authUser from '../middleware/authMiddleware.js';
 
 const cartRouter = express.Router();
 //Cart Routes
+cartRouter.get('/get_cart', authUser, get_cart); // Get cart items
 cartRouter.post('/update_cart', authUser, update_cart); // Update cart
 
 export default cartRouter;
